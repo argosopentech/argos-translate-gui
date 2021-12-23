56e93d824bb378707e522d17d4474eef5abb3119
P.J. Finlay <pjappdevelopment@gmail.com>
2021-12-22 18:07:42 -0600

    Added WorkerThread

--- argostranslategui/gui.py
+++ argostranslategui/gui.py
@@ -10,6 +10,20 @@ from PyQt5.QtCore import *
 from argostranslate import translate, package, settings, utils
 from argostranslate.utils import info, error
 
+class WorkerThread(QThread):
+    """Runs a bound function on a thread"""
+
+    def __init__(self, bound_worker_function):
+        """Args:
+        bound_worker_function (functools.partial)
+        """
+        super().__init__()
+        self.bound_worker_function = bound_worker_function
+        self.finished.connect(self.deleteLater)
+
+    def run(self):
+        self.bound_worker_function()
+
 
 class TranslationThread(QThread):
     send_text_update = pyqtSignal(str)
